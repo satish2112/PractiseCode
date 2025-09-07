e12cbde37e4da73567f12727761789db88888fe8
satish2112 <satishdineshgupta@gmail.com>
2025-09-08 00:45:37 +0530

    Initial commit - practice problems setup

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,40 @@
+public class AddTwoNumbers {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {
+        }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);  // dummy head
+        ListNode curr = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            int total = val1 + val2 + carry;
+            carry = total / 10;
+
+            curr.next = new ListNode(total % 10);
+            curr = curr.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/RomanToInteger.java
@@ -0,0 +1,35 @@
+public class RomanToInteger {
+    private static int valueOf(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
+    public static int romanToInt(String s) {
+        int result = 0;
+        int prev = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int curr = valueOf(s.charAt(i));
+            if (curr < prev) {
+                result -= curr;
+            } else {
+                result += curr;
+            }
+            prev = curr;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "XXXI";
+        System.out.println(romanToInt(s));
+    }
+}
